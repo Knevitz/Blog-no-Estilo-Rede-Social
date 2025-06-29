1affd7fde13201089f8abb238d2345868db9995e
j0a0Henrique <joaorosamoto4@gmail.com>
2025-06-29 14:19:25 -0300

    commit-5

--- backend/src/controller/postcontroller.js
+++ backend/src/controller/postcontroller.js
@@ -1,5 +1,6 @@
 const PostModel = require('../models/postModel');
 
+// Criar post
 exports.createPost = async (req, res) => {
   try {
     const novoPost = await PostModel.create(req.body);
@@ -9,6 +10,7 @@ exports.createPost = async (req, res) => {
   }
 };
 
+// Listar todos os posts
 exports.getPosts = async (req, res) => {
   try {
     const posts = await PostModel.getAll();
@@ -17,6 +19,8 @@ exports.getPosts = async (req, res) => {
     res.status(500).json({ erro: 'Erro ao buscar posts', detalhes: err.message });
   }
 };
+
+// Buscar post por ID
 exports.getPostById = async (req, res) => {
   const { id } = req.params;
   try {
@@ -28,4 +32,33 @@ exports.getPostById = async (req, res) => {
   } catch (err) {
     res.status(500).json({ erro: 'Erro ao buscar post', detalhes: err.message });
   }
-};  
+};
+
+// Deletar post
+exports.deletePost = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const resultado = await PostModel.delete(id);
+    if (!resultado) {
+      return res.status(404).json({ error: 'Post não encontrado' });
+    }
+    res.status(200).json({ message: 'Post deletado com sucesso' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao deletar post' });
+  }
+};
+
+// Curtir post
+exports.likePost = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await PostModel.like(id);
+    if (!post) {
+      return res.status(404).json({ error: 'Post não encontrado' });
+    }
+    res.json({ message: 'Curtida adicionada com sucesso', post });
+  } catch (err) {
+    res.status(500).json({ error: 'Erro ao curtir o post', detalhes: err.message });
+  }
+};
--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const postController = require('../controller/postController');
-const verificarToken = require('../middleware/middleware.js');
+const postController = require('.../postController');
+const verificarToken = require('../middleware/middleware');
 
-router.post('/posts', postController.createPost);
+router.post('/posts', verificarToken, postController.createPost);
 router.get('/posts', postController.getPosts);
+router.get('/posts/:id', postController.getPostById);
+router.delete('/posts/:id', verificarToken, postController.deletePost);
+router.patch('/posts/:id/like', postController.likePost);
 
 module.exports = router;
